be967e9bb2f54dd787a2cf599bcabfbbde1733a5
Narayanan, Rasika <rasika.narayanan@disneystreaming.com>
2023-06-10 12:31:25 -0700

    Creating conttroller

---
+++ src/main/java/com/aol/ai/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.aol.ai.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Home Controller.
+ */
+@RestController
+@RequestMapping("/")
+public class HomeController {
+}
